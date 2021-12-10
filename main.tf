dfe4309c30b851b97cb05ca409bb0b464dff2130
Jonathan Oliveira Crispiniano <68307807+jowcrisp@users.noreply.github.com>
2021-12-09 22:29:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,14 +31,6 @@ resource "aws_key_pair" "test_key" {
   public_key = tls_private_key.test_tls.public_key_openssh
 }
 
-resource "aws_network_interface" "test_interface" {
-  subnet_id = "subnet-055776e7cd7e652da"
-
-  tags = {
-    Name = "test-interface"
-  }
-}
-
 resource "aws_security_group" "test_sg" {
   name = "test-sg"
   ingress {
@@ -58,11 +50,6 @@ resource "aws_instance" "test_instance" {
               #!/bin/bash
               curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && sudo apt-get install nginx -y
               EOF
-
-  network_interface {
-    network_interface_id = aws_network_interface.test_interface.id
-    device_index         = 0
-  }
 
   tags = {
     Name = "test-instance"
