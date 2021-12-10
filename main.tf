279bb34030b9807a2e56d861287b0bc430aaa616
Jonathan Oliveira Crispiniano <68307807+jowcrisp@users.noreply.github.com>
2021-12-09 22:43:02 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,13 +31,11 @@ resource "aws_key_pair" "test_key" {
   public_key = tls_private_key.test_tls.public_key_openssh
 }
 
-resource "aws_security_group" "test_sg" {
-  name = "test-sg"
-  ingress {
-    from_port   = 22 
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_network_interface" "test_interface" {
+  subnet_id = "subnet-055776e7cd7e652da"
+
+  tags = {
+    Name = "test-interface"
   }
 }
 
@@ -45,11 +43,15 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-0629230e074c580f2"
   instance_type = "t2.micro"
   key_name      = "test-key" 
-  vpc_security_group_ids  = [aws_security_group.test_sg.id]
   user_data = <<-EOF
               #!/bin/bash
               curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && sudo apt-get install nginx -y
               EOF
+
+  network_interface {
+    network_interface_id = aws_network_interface.test_interface.id
+    device_index         = 0
+  }
 
   tags = {
     Name = "test-instance"
