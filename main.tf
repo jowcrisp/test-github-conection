b2e276cd49012a7e8378c8c1e1c1b7954f4f2b0d
Jonathan Oliveira Crispiniano <68307807+jowcrisp@users.noreply.github.com>
2021-12-10 00:14:06 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "test_sg" {
 resource "aws_instance" "test_instance" {
   ami           = "ami-0629230e074c580f2"
   instance_type = "t2.micro"
-  key_name      = "test-key" 
+  key_name      = aws_key_pair.test_key.key_name
   vpc_security_group_ids  = [aws_security_group.test_sg.id]
   user_data = <<-EOF
               #!/bin/bash
